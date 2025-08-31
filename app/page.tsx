2b9ba9da228120bdfeb5d5e7bd824b82ed6003b0
Shreeyam Karan <shreeyam.karan@beehyv.com>
2025-08-31 21:48:26 +0530

    Add clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <div className="bg-red-50 text-2xl">Hello world</div>;
+import { prisma } from "@/lib/prisma";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+
+export default async function Home() {
+  const resumes = await prisma.resume.findMany();
+  console.log(resumes);
+  return (
+    <div className="">
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton>
+          <button className="text-ceramic-white h-10 cursor-pointer rounded-full bg-[#6c47ff] px-4 text-sm font-medium sm:h-12 sm:px-5 sm:text-base">
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 }
